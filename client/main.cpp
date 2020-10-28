a756183cf51e31bb828b7284aff592ccd9d27eb8
RomanoFelix16 <43682358+RomanoFelix16@users.noreply.github.com>
2020-10-27 21:08:59 -0300

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -3,21 +3,6 @@
 #include <string>
 #include <sstream>
 
-
-#include <windef.h>
-#include <winbase.h>
-#include <winuser.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ws2tcpip.h>
-
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <windows.h>
-
 using namespace std;
 
 class Client{
@@ -63,51 +48,15 @@ public:
         fflush(stdin);
         cin>>password;
         if(checkConnectivity()== -1) return -1;
-        /*ioctlsocket(server,FIONBIO,&iMode);
-        struct timeval timeout;
-        fd_set read_fds;
-        //fd_set write_fds;
-        //fd_set except_fds;
-        int maxfd = 0;
-        FD_ZERO(&read_fds);
-        FD_SET(maxfd,&read_fds);
-        timeout.tv_sec = 1;
-        timeout.tv_usec = 0;
-        //build_fd_sets(&server, &read_fds, &write_fds, &except_fds);
-        int activity = select(maxfd+1, &read_fds, NULL, NULL, &timeout);
-        cout<<"Error "<<WSAGetLastError()<<endl;*/
         Enviar(user);
         Enviar(password);
         strcpy(mensaje,Recibir());
-
-        /*cout<<"Usuario recibido: "<<user<<endl;
-        Enviar(password);
-        strcpy(password,Recibir());
-        cout<<"Password recibido: "<<password<<endl;*/
-        //cout<<"Usuario y password recibidos!\n";
         if(strcmp(mensaje,"LOGIN_VALID")==0){
             return 0;
         }else{
             return 1;
         }
     }
-    /*int build_fd_sets(SOCKET *servidor, fd_set *read_fds, fd_set *write_fds, fd_set *except_fds)
-    {
-      FD_ZERO(read_fds);
-      FD_SET(STDIN_FILENO, read_fds);
-      FD_SET(*servidor, read_fds);
-
-      FD_ZERO(write_fds);
-      // there is smth to send, set up write_fd for server socket
-      if (buffer > 0)
-        FD_SET(*servidor, write_fds);
-
-      FD_ZERO(except_fds);
-      FD_SET(STDIN_FILENO, except_fds);
-      FD_SET(*servidor, except_fds);
-
-      return 0;
-    }*/
     int checkConnectivity(){ /** Sirve para comprobar si el servidor esta apto para recibir y enviar mensajes**/
         int isConnected = 0;
         u_long iMode= 1;
@@ -120,37 +69,22 @@ public:
         ioctlsocket(server,FIONBIO,&iMode); //Y viceversa
         return isConnected;
     }
-    int Enviar(const char* data)
-    {
-        int envio;
-        //cout<<"Escribe el mensaje a enviar: ";
-        //cin>>this->buffer;
+    void Enviar(const char* data){
         strcpy(this->buffer,data);
-        envio = send(server, buffer, sizeof(buffer), 0);
-        //memset(buffer, 0, sizeof(buffer));
-        //cout << "Mensaje enviado!" << endl;
-        return envio;
+        send(server, buffer, sizeof(buffer), 0);
     }
-    char* Recibir()
-    {
-
+    char* Recibir(){
         char* temp;
         recv(server, buffer, sizeof(buffer), 0);
         temp = buffer;
-        //cout << "El servidor dice: " << buffer << endl;
-        //memset(buffer, 0, sizeof(buffer));
         cout<<"MENSAJE RECIBIDO: "<<temp<<"\n";
         return temp;
     }
-    void CerrarSocket()
-    {
+    void CerrarSocket(){
        closesocket(server);
        WSACleanup();
        cout << "Socket cerrado." << endl << endl;
     }
-
-
-
 
     void menu (){
 
@@ -160,161 +94,44 @@ public:
 
         cout<<"\n1-ALTA SERVICIO: \n2-GESTIONAR PASAJES:\n3-REGISTRO DE ACTIVIDADES:\n4-CERRAR SESION:\n";
         cin>>opcion;
-
-
+        if(checkConnectivity()== -1){cout<<"Cliente desconectado por inactividad\n";system("pause");exit(EXIT_SUCCESS);}
         switch(opcion){
 
          case 1:
             cout<<"USTED SELECCIONO LA OPCION ALTAS\n\n";
+                Enviar("1");
                 Altas();
+                menu();
                 break;
         case 2:
             cout<<"USTED SELECCIONO LA OPCION GESTIONAR PAQUETES\n";
-                //Gestionar paquetes();
+                //Gestionar paquetes();opcion=0;
+                Enviar("2");
                 break;
         case 3:
             cout<<"USTED SELECCIONO REGISTRO DE ACTIVIDADES\n";
-                //Altas();
+                //Altas();opcion=0;
+                Enviar("3");
                 break;
         case 4:
             cout<<"CERRAR SESION\n";
+                Enviar("4");
                 CerrarSocket();
                 break;
        default:
-        {
             cout<<"SELECCIONE UN OPCION CORRECTA\n";
             break;
         }
-
         }
-        }
-
-    system ("pause");
-    system("cls");
-
     }
 
-void Reserva(){
+void Altas(){
     char origen[15];
     int dia;
     int mes;
     int anio;
     char turno[10];
-    int opcion=0; int opcion1=0; int opcion2=0;
-    cout<<"INGRESE ORIGEN, FECHA Y TURNO DEL SERVICIO QUE DESEA:\n";
-    cout<<"INGRESE EL ORIGEN:\n\n1-Buenos Aires\n2-Mar del plata\n3-SALIR\n";
-    while (opcion <1 || opcion>3){
-    cin>>opcion;
-
-    switch(opcion){
-
-    case 1:
-    cout<<"USTED HA SELECCIONADO ORIGEN BUENOS AIRES\n\n";
-        strcpy(origen,"Buenos Aires");
-            break;
-        case 2:
-            cout<<"USTED HA SELECCIONADO ORIGEN MAR DEL PLATA\n\n";
-               strcpy(origen,"Mar del Plata");
-                break;
-        case 3:
-            cout<<"SALIR \n";
-                system("cls");
-                return;
-                //menu();
-                break;
-       default:
-            cout<<"SELECCIONE UNA OPCION CORRECTA\n";
-            break;
-    }
-
-    }
-fflush(stdin);
-cout<<"INGRESE EL DIA: ";
-cin>>dia;
-cout<<"INGRESE EL MES: ";
-cin>>mes;
-cout<<"INGRESE EL ANIO: ";
-cin>>anio;
-ostringstream os;
-os << dia << '/' << mes << '/' << anio;
-//cout << os.str();
-string fecha=os.str();
-cout<<"LA FECHA ES: "<<fecha<<"\n";
-fflush(stdin);
-
-char date[15];
-strcpy(date,fecha.c_str());
-
-cout<<"\nINGRESE EL TURNO:\n1-MANANA\n2-TARDE\n3-NOCHE\n4-SALIR\n";
-while (opcion1 <1 || opcion1>4){
-    cin>>opcion1;
-
-    switch(opcion1){
-
-    case 1:
-    cout<<"USTED HA SELECCIONADO TURNO MANANA\n";
-        strcpy(turno,"maniana");
-            break;
-        case 2:
-            cout<<"USTED HA SELECCIONADO TURNO TARDE\n";
-               strcpy(turno,"tarde");
-                break;
-        case 3:
-            cout<<"USTED HA SELECCIONADO TURNO NOCHE\n";
-               strcpy(turno,"noche");
-                break;
-        case 4:
-            cout<<"SALIR \n";
-                system("cls");
-                return;
-                //menu();
-                break;
-       default:
-            cout<<"SELECCIONE UN OPCION CORRECTA\n";
-            break;
-    }
-
-    }
-
-
-
-    cout<<"\nEL SERVICIO SELECCIONADO ES:\n"<<"ORIGEN: "<<origen<<" - FECHA:"<<date<<" - TURNO:"<<turno<<".\n\n";
-
-
-
-    cout<<"\n1-RESERVAR UN ASIENTO: \n2-LIBERAR UN ASIENTO: \n3- ELEGIR OTRO SERVICIO: \n4-VOLVER AL MENÚ ANTERIOR:\n";
-    cin>>opcion2;
-
-    switch(opcion2){
-
-    case 1:
-        cout<<"USTED SELECCIONO RESERVAR UN ASIENTO\n";
-        break;
-    case 2:
-        cout<<"USTED SELECCIONO LIBERAR UN ASIENTO\n";
-        break;
-    case 3:
-        cout<<"USTED SELECCIONO ELEGIR OTRO SERVICIO\n";
-    case 4:
-        menu();
-        break;
-
-        }
-    }
-
-
-}
-    
-int Altas(){
-
-    char origen[15];
-    int dia;
-    int mes;
-    int anio;
-    char turno[10];
-    int env;
     int opcion=0;int opcion1=0;
-
     cout<<"INGRESE EL ORIGEN:\n\n1-Buenos Aires\n2-Mar del plata\n3-SALIR\n";
     while (opcion <1 || opcion>3){
     cin>>opcion;
@@ -331,17 +148,15 @@ int Altas(){
                 break;
         case 3:
             cout<<"SALIR \n";
-                menu();
+                system("cls");
+                return;
                 break;
        default:
-        {
             cout<<"SELECCIONE UNA OPCION CORRECTA\n";
             break;
-        }
     }
 
     }
-
 fflush(stdin);
 cout<<"INGRESE EL DIA: ";
 cin>>dia;
@@ -359,7 +174,6 @@ fflush(stdin);
 char date[15];
 strcpy(date,fecha.c_str());
 
-
 cout<<"\nINGRESE EL TURNO:\n1-MANANA\n2-TARDE\n3-NOCHE\n4-SALIR\n";
 while (opcion1 <1 || opcion1>4){
     cin>>opcion1;
@@ -380,18 +194,15 @@ while (opcion1 <1 || opcion1>4){
                 break;
         case 4:
             cout<<"SALIR \n";
-                menu();
+                system("cls");
+                return;
                 break;
        default:
-        {
             cout<<"SELECCIONE UN OPCION CORRECTA\n";
             break;
-        }
     }
 
     }
-
-
 fflush(stdin);
 char alta[50]="";
 strcat(alta,origen);
@@ -399,19 +210,16 @@ strcat(alta,";");
 strcat(alta,date);
 strcat(alta,";");
 strcat(alta,turno);
+strcat(alta,";");
 
 cout<<"\nEL SERVICIO SELECCIONADO ES:\n"<<"ORIGEN: "<<origen<<" - FECHA:"<<date<<" - TURNO:"<<turno<<".\n\n";
 
-env = Enviar(alta);
+Enviar(alta);
 strcpy(alta,Recibir());
-
 
 system ("pause");
 system("cls");
-menu();
-
-
-return 0;
+return;
 }
 
 };
@@ -427,7 +235,7 @@ int main()
         cout<<"\nIngrese un puerto: ";
         cin>>port;
     }while(Cliente->_init(ip,port)!=0);
-        //Cliente->Enviar();
+
     while(intentos!=0){
         loginSuccess = Cliente->login();
         if(loginSuccess == 0 || loginSuccess == -1){
@@ -444,19 +252,12 @@ int main()
         Cliente->CerrarSocket();
     }else if(intentos > 0 && loginSuccess == 0){
         cout<<"Login successful"<<endl;
-
         Cliente->menu();
-        //Cliente->CerrarSocket();
+        system ("pause");
     }else if(loginSuccess == -1){
         cout<<"Cliente desconectado por inactividad."<<endl;
-        system("pause");
         Cliente->CerrarSocket();
+        system("pause");
     }
-    //system("pause");system("cls");
-
-    //Client *Cliente = new Client(ip,port);
-
     return 0;
 }
-
-
