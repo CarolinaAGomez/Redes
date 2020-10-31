65bba6d72398f1a30d2195661e3617eec7108877
carolinagomez.gc@gmail.com <carolinagomez.gc@gmail.com>
2020-10-31 16:41:44 -0300

    Lectura externa

---
+++ LecturaArchivo/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+using namespace std;
+
+struct Servicio{
+    char origen[64];
+    char fecha[64];
+    char turno[64];
+    char asientos[64];
+};
+
+void lectura();
+void escritura();
+
+int main()
+{
+
+    escritura();
+    lectura();
+
+}
+
+void lectura(){
+
+ofstream altaLegible("Legible.txt",ios::out | ios::app);
+ifstream lecturaLegible("Legible.txt",ios::in | ios::app);
+ string lector;
+  getline(lecturaLegible,lector,';');
+        while(!lecturaLegible.eof()){
+
+
+        cout<<lector<<"\n";
+        getline(lecturaLegible,lector,';');
+
+        }
+
+
+        altaLegible.close();
+        lecturaLegible.close();
+
+
+
+
+}
+
+void escritura(){
+
+
+    ifstream altas("C:\\Users\\Caroo Gomez\\Desktop\\UNLA\\Redes\\Redes\\server\\Altas.bin",ios::out | ios::binary);
+    ofstream altaLegible("Legible.txt",ios::out | ios::app);
+
+
+       Servicio aux;
+
+        altas.read((char*)(&aux),sizeof(Servicio));
+
+
+        while(!altas.eof() ){
+
+        altaLegible<<"Origen:" <<aux.origen<<"- Fecha:" <<aux.fecha<<"- Turno:" <<aux.turno<<"- Ascientos:" <<aux.asientos<<";\n";
+         altas.read((char*)(&aux),sizeof(Servicio));
+        }
+
+        altas.close();
+}
